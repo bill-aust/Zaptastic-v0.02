781bdbe200181b61af58f42c892667f74f456fd8
Billy Jordan <2bbilly.jordan@gmail.com>
2020-04-22 21:52:50 +1000

    Initial Commit

---
+++ Zaptastic v0.02Tests/Zaptastic_v0_02Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Zaptastic_v0_02Tests.swift
+//  Zaptastic v0.02Tests
+//
+//  Created by Bill Jordan on 22/4/20.
+//  Copyright © 2020 Bill Jordan. All rights reserved.
+//
+
+import XCTest
+@testable import Zaptastic_v0_02
+
+class Zaptastic_v0_02Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
